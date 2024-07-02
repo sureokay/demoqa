2e6e99b9f5d2c4c4b712782f82dec1ce0619adfb
Bhargav <165127034+sureokay@users.noreply.github.com>
2024-07-02 23:16:28 +0530

    example spec file removed

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,7 +51,8 @@ export default defineConfig({
 
     {
       name: 'webkit',
-      use: {...devices['Desktop Safari'],
+      use: {
+        ...devices['Desktop Safari'],
         headless: true,
       },
     },
